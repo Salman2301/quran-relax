a94198c2dab7df9818179e13e19a615c4cd1c665
Salman <salmanhammed77@gmail.com>
2024-04-20 10:46:18 +0530

    add data reciter info

---
+++ src/lib/components/Sounds/data.ts
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    name: "Light Rain Ambient",
+    producer: "SoundsForYou",
+    duration: "9:00",
+    id: "rain"
+  },
+  {
+    name: "Wind Outside | Sound Ambient",
+    producer: "SoundsForYou",
+    duration: "1:29",
+    id: "wind"
+  },
+  {
+    name: "Natural Thunder",
+    producer: "SoundsForYou",
+    duration: "1:08",
+    id: "thunder"
+  },
+  {
+    name: "Campfire Crackling | Fireplace Sound",
+    producer: "SoundsForYou",
+    duration: "2:19",
+    id: "campfire"
+  },
+  {
+    name: "Calm River Ambience | Loop",
+    producer: "SoundsForYou",
+    duration: "2:00",
+    id: "river"
+  },
+  {
+    name: "Relaxing Ocean Waves - (High Quality Recorded)",
+    producer: "Prem_Adhikary",
+    duration: "3:01",
+    id: "ocean"
+  }
+]
