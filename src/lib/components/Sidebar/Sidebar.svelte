5c0f37a8260ae7e69dca655f57a827005ae5d6e2
Salman <salmanhammed77@gmail.com>
2024-04-22 18:11:11 +0530

    add sidebar animation

--- src/lib/components/Sidebar/Sidebar.svelte
+++ src/lib/components/Sidebar/Sidebar.svelte
@@ -22,13 +22,19 @@
 	.sidebar-container {
 		position: fixed;
 		top: 0;
-		right: 0;
+		right: 0px;
 		width: 400px;
 		height: 100vh;
 		background-color: #1b1c21;
 		padding: 10px 20px;
 		z-index: 1000;
 		border-left: 1px solid white;
+		animation: slide 0.4s ease;
+	}
+
+	@keyframes slide {
+		0% { right: -400px; }
+		100% { right: 0; }
 	}
 	.scroll {
 		overflow-y: auto;
